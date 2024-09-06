176ae123d53afbb228611fcd9da7a5eaa3fa242c
Sreerag Rajesh Babu <Sreerag.Babu@asl.mum.sst>
2024-09-06 22:07:59 +0530

    v1

---
+++ SessionTimeoutPOC/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SessionTimeoutPOC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult SetSession()
+        {
+            Session["UserName"] = "Sreerag"; 
+            return Content("Session data set. UserName: Sreerag");
+        }
+
+        public ActionResult GetSession()
+        {
+            var userName = Session["UserName"];
+
+            if (userName == null)
+            {
+                return Content("Session has expired or not set.");
+            }
+
+            return Content("Session data retrieved. UserName: " + userName);
+        }
+
+        public ActionResult ClearSession()
+        {
+            Session.Clear();  
+            return Content("Session data cleared.");
+        }
+    }
+}
